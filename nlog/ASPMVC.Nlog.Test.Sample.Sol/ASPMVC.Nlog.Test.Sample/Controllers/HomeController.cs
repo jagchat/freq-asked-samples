217a6aa4f1e648a657757b64a5f0600fddfcd2c1
jagchat <jag.chat@gmail.com>
2017-04-27 12:35:11 -0500

    samples added

---
+++ nlog/ASPMVC.Nlog.Test.Sample.Sol/ASPMVC.Nlog.Test.Sample/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPMVC.Nlog.Test.Sample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        // GET: Home
+        public ActionResult Index()
+        {
+            logger.Trace("this is to trace log file!");
+            logger.Error("this is to error log file!");
+            return View();
+        }
+    }
+}
---
+++ nlog/ASPMVC.Nlog.Test.Sample.Sol/ASPMVC.Nlog.Test.Sample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASPMVC.Nlog.Test.Sample.MvcApplication" Language="C#" %>
