1d78845a787011f322dc1e90dd74eb6e443ae958
Jagadish Pulakhandam <jpulakhandam@personifycorp.com>
2020-06-10 11:58:08 -0500

    Cookie Auth ASP.NET Core 2.2 sample added

---
+++ cookie-auth-using-aspnet-core-2.2/src/sample/Controllers/SecurityController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using sample.Models;
+
+namespace sample.Controllers
+{
+    public class SecurityController : Controller
+    {
+        public IActionResult Login(string returnUrl)
+        {
+            var m = new LoginViewModel() { ReturnUrl = returnUrl };
+            return View(m);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel m)
+        {
+            if (m.Email != "jag@email.com" &&
+                m.Email != "scott@email.com")
+            {
+                var nm = new LoginViewModel() { Email = m.Email, ReturnUrl = m.ReturnUrl, ErrorInfo = "Invalid UserId/Password" };
+                return View(nm);
+            }
+
+            List<Claim> claims = null;
+            if (m.Email == "jag@email.com") //admin claims
+            {
+                claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, m.Email),
+                    new Claim("FullName", "Jag"),
+                    new Claim(ClaimTypes.Role, "Administrator"),
+                };
+            }
+            else //member claims
+            {
+                claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, m.Email),
+                    new Claim("FullName", "Scott"),
+                    new Claim(ClaimTypes.Role, "Member"),
+                    new Claim("MemberId", "scott-01-abc"),
+                };
+            }
+
+            var claimsIdentity = new ClaimsIdentity(
+                claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var authProperties = new AuthenticationProperties
+            {
+                //AllowRefresh = <bool>,
+
+                //ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
+
+                //IsPersistent = true,
+                // Whether the authentication session is persisted across 
+                // multiple requests. 
+
+                //IssuedUtc = <DateTimeOffset>,
+                // The time at which the authentication ticket was issued.
+
+                //RedirectUri = <string>
+                // The full path or absolute URI to be used as an http 
+                // redirect response value.
+            };
+
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity),
+                authProperties);
+
+            return Redirect(m.ReturnUrl);
+        }
+
+        public async Task<IActionResult> Logout(string returnUrl)
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return Redirect(returnUrl);
+        }
+
+        public IActionResult AccessDenied(string returnUrl)
+        {
+            return View();
+        }
+    }
+}
---
+++ cookie-auth-using-aspnet-core-2.2/src/sample/Startup.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace sample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services
+                .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/Security/Login"; //redirects to this during challenge (default /Account/Login)
+                    options.AccessDeniedPath = "/Security/AccessDenied"; //redirects to this if authenticated, but unauthorized (default /Account/AccessDenied)
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
