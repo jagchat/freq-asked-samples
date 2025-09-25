ed38004e9cbc9a81ff047f6d703e3157e6eee0a7
Jag <jag@test.com>
2025-09-24 20:58:26 -0500

    vue.js samples added

---
+++ vue-asp-core-samples/03-custom-component-split/demo/hello-component/playground/App.vue
@@ -0,0 +1,4 @@
+<template>
+  <hello id="demo-01"></hello>
+  <hello id="demo-02"></hello>
+</template>
---
+++ vue-asp-core-samples/03-custom-component-split/demo/hello-component/src/hello.vue
@@ -0,0 +1,3 @@
+<template src="./hello-markup.html"></template>
+<style src="./hello-style.css" scoped></style>
+<script src="./hello-script.js"></script>
